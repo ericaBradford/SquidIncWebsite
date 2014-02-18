e041889c361049ce4b3aa2549d76fc5be7fb2bd5
Erica Bradford <ebradford@student.neumont.edu>
2014-02-17 20:15:37 -0700

    All site controllers and views added

---
+++ ClericoDezign_SquidInc/ClericoDezign_SquidInc/Controllers/ComingSoonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClericoDezign_SquidInc.Controllers
+{
+    public class ComingSoonController : Controller
+    {
+        //
+        // GET: /ComingSoon/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ClericoDezign_SquidInc/ClericoDezign_SquidInc/Controllers/InvestorsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClericoDezign_SquidInc.Controllers
+{
+    public class InvestorsController : Controller
+    {
+        //
+        // GET: /Investors/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
